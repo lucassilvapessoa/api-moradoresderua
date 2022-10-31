8590f7b5f7fc7861d244d29f01e3adf46ef9574e
Lucas Da Silva Pessoa <lucas-sp2015@bol.com.br>
2022-10-31 11:25:49 -0300

    quarto commit

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,6 +1,7 @@
+const mongoURI = mongoURI = "mongodb+srv://cp3005411:lucas1997@cluster0.5kbti7i.mongodb.net/?retryWrites=true&w=majority"
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
-const client = new MongoClient(process.env.mongoURI);
+const client = new MongoClient(mongoURI);
 
 module.exports = client;
